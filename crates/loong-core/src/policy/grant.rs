ff3a1c3206250bf8a7cc1515923e3bd964153272
InuDial <inu-dial@qq.com>
2026-06-30 00:49:12 +0800

    simplify Granted<Action> fields' read

--- crates/loong-core/src/policy/grant.rs
+++ crates/loong-core/src/policy/grant.rs
@@ -1,3 +1,5 @@
+use std::marker::PhantomData;
+
 use crate::{Action, error::ExecutionError};
 
 #[derive(Clone, Copy, Debug)]
@@ -9,34 +11,20 @@ pub struct GrantId(pub u64);
 /// but only policy helpers inside core can mint one.
 #[derive(Debug)]
 pub struct Granted<A: Action> {
-    grant_id: GrantId,
+    pub grant_id: GrantId,
     // info: ActionGrantInfo, // TODO
-    action: A,
+    pub action: A,
+    /// avoid manual implementation
+    _phantom_data: PhantomData<()>,
 }
 
 impl<A: Action> Granted<A> {
     pub(super) fn new(grant_id: GrantId, /*info: ActionGrantInfo,*/ action: A) -> Self {
-        Self { grant_id, action }
-    }
-
-    #[must_use]
-    pub const fn grant_id(&self) -> GrantId {
-        self.grant_id
-    }
-
-    // #[must_use]
-    // pub const fn info(&self) -> &GrantInfo {
-    //     &self.info
-    // }
-
-    #[must_use]
-    pub const fn action(&self) -> &A {
-        &self.action
-    }
-
-    #[must_use]
-    pub fn into_action(self) -> A {
-        self.action
+        Self {
+            grant_id,
+            action,
+            _phantom_data: PhantomData,
+        }
     }
 
     pub async fn execute_with<E>(self, executor: &E) -> Result<E::Output, ExecutionError>
